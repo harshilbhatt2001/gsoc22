639297faadf2713b0161a915d782b0f8cbdb084a
Jason Kridner <jkridner@beagleboard.org>
2022-06-15 18:31:42 +0000

    Try to build!

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
-if [ -f /usr/local/bin/west ] ; then
-	echo "Installing zephyr"
-	cd /opt/source/
-	echo "west init -m <repo> --mr <tag> bcf-zephyr"
-	west init -m git@github.com:harshilbhatt2001/zephyr-jadonk.git --mr f1aa25c58c27392b20a3a8a8eb2f4e245abf04a4 bcf-zephyr
+#!/bin/bash -e -x
+mkdir -p /opt/source
+export PATH=/opt/source/bcf-zephyr/zephyr/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$HOME/.local/bin
 
-	if [ -d /opt/source/bcf-zephyr/ ] ; then
-		cd /opt/source/bcf-zephyr/
-		echo "west update"
-		west update
-		echo "west zephyr-export"
-		west zephyr-export
-		echo "pip3 install -r zephyr/scripts/requirements-base.txt"
-		pip3 install -r zephyr/scripts/requirements-base.txt
+echo "Installing zephyr"
+cd /opt/source/
+echo "west init -m <repo> --mr <tag> bcf-zephyr"
+west init -m git@github.com:harshilbhatt2001/zephyr-jadonk.git --mr f1aa25c58c27392b20a3a8a8eb2f4e245abf04a4 bcf-zephyr
 
-		export ZEPHYR_TOOLCHAIN_VAIRANT=cross-compile
-		export CROSS_COMPILE=/usr/bin/arm-none-eabi-
-		export ZEPHYR_BASE=/opt/source/bcf-zephyr/zephyr
-		export PATH=/opt/source/bcf-zephyr/zephyr/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+cd /opt/source/bcf-zephyr/
+echo "west update"
+west update
+echo "west zephyr-export"
+west zephyr-export
+echo "pip3 install -r zephyr/scripts/requirements-base.txt"
+pip3 install -r zephyr/scripts/requirements-base.txt
 
-		echo "source /opt/source/bcf-zephyr/zephyr/zephyr-env.sh" >> /root/.bashrc
-	fi
-fi
+export ZEPHYR_TOOLCHAIN_VAIRANT=cross-compile
+export CROSS_COMPILE=/usr/bin/arm-none-eabi-
+export ZEPHYR_BASE=/opt/source/bcf-zephyr/zephyr
+export BOARD=beagleconnect_freedom
+
+# Build a demo here
+west build -d build/blinky zephyr/samples/basic/blinky
